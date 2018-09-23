2b43a2ec7ccad40ea1254c8916b0f2550b775811
tylertownsend <ttown@knights.ucf.edu>
2018-09-23 19:49:32 -0400

    Implemented Serial AVL Tree

--- include/AVLTree.h
+++ include/AVLTree.h
@@ -14,9 +14,22 @@
 class AVLTree {
  private:
   Node* root;
+  Node* insert(Node* node, int const& data);
+  Node* remove(Node* node, int const& data);
+  bool contains(Node* node, int const& data);
+  Node* rotateLeft(Node* node);
+  Node* rotateRight(Node* node);
+  int findMax(Node* node);
+  int height(Node* node);
+  int balanceFactor(Node* node);
+  Node* rebalance(Node* node);
 
  public:
-
+  AVLTree();
+  ~AVLTree();
+  bool insert(int const& data);
+  void remove(int const& data);
+  bool contains(int const& data);
 
 };
 
