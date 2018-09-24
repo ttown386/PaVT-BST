00c57c9b4c5253ceaa797b6c9cf68e291d0727d5
tylertownsend <ttown@knights.ucf.edu>
2018-09-23 20:11:42 -0400

    implemented tests for node class, need more for height

--- test/TestNode.cpp
+++ test/TestNode.cpp
@@ -2,3 +2,47 @@
 // Created by ttown on 9/23/2018.
 //
 
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+
+#include "Node.h"
+
+namespace {
+class TestNode : public testing::Test {
+ protected:
+  Node * node;
+ public:
+
+   virtual void SetUp();
+   virtual void TearDown();
+};
+}
+
+void TestNode::SetUp() {
+  node = new Node(5);
+}
+
+void TestNode::TearDown() {
+  delete node;
+}
+
+TEST_F(TestNode, setData) {
+
+  ASSERT_EQ(5, node->getData());
+}
+
+TEST_F(TestNode, inserting) {
+
+  int val1 = 7;
+  int val2 = 1;
+
+  Node *test1 = new Node(val1);
+  Node *test2 = new Node(val2);
+
+  node->setLeft(test2);
+  node->setRight(test1);
+
+  ASSERT_EQ(val2, node->getLeft()->getData());
+  ASSERT_EQ(val1, node->getRight()->getData());
+}
