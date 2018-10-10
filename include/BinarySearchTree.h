6d2a074e90a02fb742a24013ee3cbf8c744e7641
tyler townsend <ttown@knights.ucf.edu>
2018-10-09 23:46:26 -0400

    Implemneted BinarySearchTree class (need to update traversal)

--- include/BinarySearchTree.h
+++ include/BinarySearchTree.h
@@ -5,7 +5,28 @@
 #ifndef CONCURRENTTREETRAVERALS_BINARYSEARCHTREE_H
 #define CONCURRENTTREETRAVERALS_BINARYSEARCHTREE_H
 
+#include "Node.h"
+
 class BinarySearchTree {
+ public:
+// private:
+  bool isAvl;
+  Node *root=nullptr;
+  void updateHeights(Node *curr);
+  Node *rotateLeft(Node *node);
+  Node *rotateRight(Node *node);
+  int findMin(Node *node);
+  int height(Node *node);
+  int balanceFactor(Node *node);
+  void rebalance(Node *node);
+
+// public:
+  BinarySearchTree(bool isAvl=false);
+  ~BinarySearchTree();
+  void insert(int const& data);
+  void remove(int const& data);
+  bool contains(int const& data);
+  Node *getRoot();
 
 };
 
--- src/serial/BinarySearchTree.cc
+++ src/serial/BinarySearchTree.cc
@@ -2,4 +2,254 @@
 // Created by ttown on 9/30/2018.
 //
 
-#include "BinarySearchTree.h"
+#include "../../include/BinarySearchTree.h"
+#include <algorithm>
+#include <iostream>
+
+const int MAXBF = 1;
+const int MINBF = -1;
+
+BinarySearchTree::BinarySearchTree(bool isAvl) {
+  this->isAvl = isAvl;
+}
+
+BinarySearchTree::~BinarySearchTree() {
+  delete root;
+}
+
+Node *BinarySearchTree::getRoot() {
+  return root;
+}
+
+void BinarySearchTree::insert(int const &data) {
+  // Tree is empty
+  if (root == nullptr) {
+    root = new Node(data);
+    root->setParent(nullptr);
+    return;
+  }
+
+  // Otherwise we traverse
+  Node *curr = root;
+  Node *parent = nullptr;
+
+  // traverse
+  while (true) {
+
+    // We have a duplicate
+    // TODO incorperate for External BST
+    if (curr->getData()==data) {
+      return;
+    }
+
+    // update parent;
+    parent = curr;
+
+    // traverse to next child
+    bool parentIsLarger= data < parent->getData();
+    curr = (parentIsLarger ? curr->getLeft() : curr->getRight());
+
+    // check if the current node is a leaf
+    if (curr == nullptr) {
+
+      curr = new Node(data);
+      // Get the height of the new node
+      curr->setHeight(0);
+
+      // update the parent of new node
+      curr->setParent(parent);
+
+      // If the parent is larger than the child, set the left pointer
+      // of parent to be the new node
+      if (parentIsLarger) {
+        parent->setLeft(curr);
+      } else {
+        parent->setRight(curr);
+      }
+      // Exit the initial traversal
+      break;
+    }
+
+  }
+
+  // Update heights
+  updateHeights(curr);
+  // Perform AVL rotations if applicable
+  if (isAvl){
+    rebalance(curr);
+  }
+}
+
+void BinarySearchTree::remove(int const &data) {
+
+}
+
+bool BinarySearchTree::contains(int const &data) {
+  return false;
+}
+
+void BinarySearchTree::updateHeights(Node *curr) {
+
+  // Get a copy of curr to not lose its reference in
+  // other calls
+  Node *temp = curr;
+
+  while (temp!=nullptr) {
+    // update height of the subtree rooted here
+    Node *left = temp->getLeft();
+    Node *right = temp->getRight();
+    int maxSubHeight = std::max(height(left), height(right));
+    temp->setHeight(1 + maxSubHeight);
+
+    // go to the next parent
+    temp = temp->getParent();
+  }
+}
+
+Node *BinarySearchTree::rotateLeft(Node *node) {
+
+  // Grab the nodes right child
+  Node *newRoot = node->getRight();
+
+  // Give node the left child of the rotated node since the
+  // key is greater than node
+  Node *temp = newRoot->getLeft();
+  node->setRight(temp);
+
+  // The node's right child (temp) now moves up to take the place of
+  // node
+  newRoot->setLeft(node);
+
+  // Update parents
+  if(temp!=nullptr) temp->setParent(node);
+
+  Node *rootParent = node->getParent();
+  if (rootParent!=nullptr) {
+    if (rootParent->getRight() == node) {
+      rootParent->setRight(newRoot);
+    } else {
+      rootParent->setLeft(newRoot);
+    }
+  }
+  newRoot->setParent(rootParent);
+  node->setParent(newRoot);
+
+  // Update the tree heights
+  int leftHeight = height(node->getLeft());
+  int rightHeight = height(node->getRight());
+  node->setHeight(1 + std::max(leftHeight, rightHeight));
+
+  int newRootLeftHeight = height(newRoot->getLeft());
+  int newRootRightHeight = height(newRoot->getRight());
+  newRoot->setHeight(1 + std::max(newRootLeftHeight, newRootRightHeight));
+
+  return newRoot;
+}
+
+Node *BinarySearchTree::rotateRight(Node *node) {
+
+  // Grab the nodes left child
+  Node* newRoot = node->getLeft();
+
+  // Give node the left child of newRoot since the key
+  // is less than node
+  Node *temp = newRoot->getRight();
+  node->setLeft(temp);
+
+  // The new Root moves up to take the place of node
+  // Now set newNodes right pointer to node
+  newRoot->setRight(node);
+
+  // Update parents
+  if(temp!=nullptr) temp->setParent(node);
+
+  Node *rootParent = node->getParent();
+  if (rootParent!=nullptr) {
+    if (rootParent->getRight() == node) {
+      rootParent->setRight(newRoot);
+    } else {
+      rootParent->setLeft(newRoot);
+    }
+  }
+  newRoot->setParent(rootParent);
+  node->setParent(newRoot);
+
+  // Update the tree heights
+  int leftHeight = height(node->getLeft());
+  int rightHeight = height(node->getRight());
+  node->setHeight(1 + std::max(leftHeight, rightHeight));
+
+  int newRootLeftHeight = height(newRoot->getLeft());
+  int newRootRightHeight = height(newRoot->getRight());
+  newRoot->setHeight(1 + std::max(newRootLeftHeight, newRootRightHeight));
+
+  return newRoot;
+}
+
+int BinarySearchTree::findMin(Node *node) {
+  return 0;
+}
+
+/*
+ * Returns the height of node
+ */
+int BinarySearchTree::height(Node *node) {
+  return (node == nullptr) ? -1 : node->getHeight();
+}
+
+/*
+ * Returns the balance factor of node
+ * @param node
+ * @return
+ */
+int BinarySearchTree::balanceFactor(Node *node) {
+  int hLeft = height(node->getLeft());
+  int hRight = height(node->getRight());
+  return hLeft - hRight;
+}
+
+/*
+ * Check the balance factor at this node, it does not meet requirements
+ * perform tree rebalance
+ *
+ * @param node
+ * @return
+ */
+void BinarySearchTree::rebalance(Node *node) {
+
+  // get balance factor
+  int bf = balanceFactor(node);
+
+  // The node's right subtree is too tall
+  if (bf < MINBF) {
+
+    // If the node's right subtree is left heavy, then
+    // the subtree must be rotated to the right
+    if (balanceFactor(node->getRight()) > 0)
+      node->setRight(rotateRight(node->getRight()));
+
+    // We rotate left when the node's right subtree is right- heavy
+    // This will also account if the node's right node is also has
+    // and right-heavy subtree.
+    node = rotateLeft(node);
+
+    // The node's left subtree is too tall
+  } else if (bf > MAXBF) {
+
+    // If the node's left subtree is right heavy, then
+    // the subtree must be rotated to the left
+    if (balanceFactor(node->getLeft()) < 0)
+      node->setLeft(rotateLeft(node->getLeft()));
+
+    // We rotate right when the node's left subtree is left-heavy
+    // This will also account if the node's left node is also has
+    // and left-heavy subtree.
+    node = rotateRight(node);
+  }
+  // TODO : make this iterative
+  if (node->getParent()!=nullptr) {
+    rebalance(node->getParent());
+  } else {
+    root = node;
+  }
+}
