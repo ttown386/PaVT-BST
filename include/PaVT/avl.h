967f5bb5f39699115ac5798a2bbdda8fb0ebe685
Tyler S. Townsend <ttown@knights.ucf.edu>
2019-03-23 19:27:47 -0400

    implemented avl class that extends bst

---
+++ include/PaVT/avl.h
@@ -0,0 +1,19 @@
+#ifndef PAVT_AVL_TREE_H_
+#define PAVT_AVL_TREE_H_
+
+#include <PaVT/PaVTBST.h>
+
+namespace pavt {
+
+class AVL : public PaVTBST {
+ public:
+  void insert(const int& key);
+  void remove(const int& key);
+ protected:
+  void rotateLeft(Node* child, Node* node, Node* parent);
+  void rotateRight(Node* child, Node* node, Node* parent);
+  int height(Node* node);
+  void rebalance(Node* node);
+};
+}
+#endif // PAVT_AVL_TREE_H_
